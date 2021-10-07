410a9f74448e41e299aa4b5836e2e70a174aa88d
Mick Hill <mickhill@hotmail.com.br>
2021-10-06 21:42:11 -0300

    Implementando classe que ira comunicar com navegador

---
+++ src/main/java/support/BrowserFactory.java
@@ -0,0 +1,58 @@
+package support;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class BrowserFactory
+{
+    public static enum Browser
+    {
+        CHROME,
+        CHROME_HEADLESS,
+        FIREFOX,
+        FIREFOX_HEADLESS
+    }
+
+    public static WebDriver getBrownser()
+    {
+        return getBrownser(Browser.CHROME); // Browser Default
+    }
+
+    public static WebDriver getBrownser(Browser browserUser)
+    {
+        WebDriver resultBrowser;
+        // System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\webdrivers\\chromedriver.exe");
+        // System.setProperty("webdriver.gecko.driver",  "src\\test\\resources\\webdrivers\\geckodriver.exe");
+
+        switch (browserUser)
+        {
+            case CHROME:
+                resultBrowser = new ChromeDriver();
+                resultBrowser.manage().window().maximize();
+                break;
+            case CHROME_HEADLESS:
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--headless");
+                options.addArguments("window-size=1920x1080");
+                resultBrowser = new ChromeDriver(options);
+                break;
+            case FIREFOX:
+                resultBrowser = new FirefoxDriver();
+                resultBrowser.manage().window().maximize();
+                break;
+            case FIREFOX_HEADLESS:
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--headless");
+                firefoxOptions.addArguments("--window-size=1920,1080");
+                resultBrowser = new FirefoxDriver(firefoxOptions);
+                break;
+            default:
+                resultBrowser = null;
+                new Exception("Browser Nao Suportado");
+        }
+        return resultBrowser;
+    }
+}
